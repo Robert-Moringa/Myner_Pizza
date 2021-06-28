fe432c76ce8c6ffaaaf220dc58a72d6730ae4d19
Robert Maina <robert.maina@student.moringaschool.com>
2021-06-28 06:19:00 +0300

    used DOM methods to display total

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//business logic
+// function Order(name, total, delivery) {
+//     this.orderName = name;
+//     this.total = total;
+//     this.delivery = delivery;
+//   }
+  
+  
+//   Order.prototype.wholeOrder = function() {
+//     return this.orderName;
+//   }
+  
+
+
+
+function total(){
+    var name = document.getElementById("flavor").value;
+
+    var price = parseInt(document.getElementById("size").value);
+
+    var crust = parseInt(document.getElementById("crust").value);
+
+    var toppings= 60;
+
+    var delivery = parseInt(document.getElementById("delivery").value);
+
+    var total = price+crust+toppings+delivery;
+
+    var name = document.getElementById("flavor").value;
+
+    document.getElementById("order-name").innerHTML = "Pizza flavor: " + name;
+
+    document.getElementById("total").innerHTML = "Total: Kshs. "+ total;
+};
+total();
+
+
+$(document).ready(function(){
+    $("#place-order").submit(function(){
+        var location= $("#place").val();
+        alert("We have received your order. We will deliver it at " + location);
+      });
+         
+});
